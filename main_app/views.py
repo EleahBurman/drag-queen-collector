9857f7a9dc79db2a11ceeeaddd81ff8bf3935a9f
EleahBurman <eleahjburman@gmail.com>
2023-06-02 15:36:07 -0400

    Render data for index in template

--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,20 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+class Queen:
+  def __init__(self, name, season, winner, allstars, winnerofallstars, specialty, instagramhandle):
+    self.name = name
+    self.season = season
+    self.winner = winner
+    self.allstars = allstars
+    self.winnerofallstars = winnerofallstars
+    self.specialty = specialty
+    self.instagramhandle = instagramhandle
+
+dragqueens = [
+  Queen('Katya', 7, False, 2, False, 'Splits', 'katya_zamo')
+]
+
+def dragqueen_index(request):
+  return render(request, 'dragqueens/index.html', {'dragqueens': dragqueens })
