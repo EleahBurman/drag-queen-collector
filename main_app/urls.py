85d1c9212d6f9436afc675b21b1778220c5a1bb7
EleahBurman <eleahjburman@gmail.com>
2023-06-05 15:41:32 -0400

    Create login view for home page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('dragqueens/', views.dragqueen_index, name='dragqueen-index'),
   path('dragqueens/<int:dragqueen_id>/', views.dragqueen_detail, name='dragqueen-detail'),
